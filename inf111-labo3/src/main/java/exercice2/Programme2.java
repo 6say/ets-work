d8710b967e74a7628ae6bf621ce34dddb58178d8
Ndiaga Cissé <cndiaga2002@gmail.com>
2025-09-29 22:31:42 -0400

    Complete Labo3, ready to merge

--- inf111-labo3/src/main/java/exercice2/Programme2.java
+++ inf111-labo3/src/main/java/exercice2/Programme2.java
@@ -1,18 +1,21 @@
 package exercice2;
 
 import java.lang.reflect.Array;
+import java.util.Scanner;
 
 public class Programme2 {
 	public static void main(String[] args) {
-		String[] fruits = {"Poires","Figues","Grenadines","Prunes","Mangues",
-				"Raisins","Mandarines","Pommes","Melons","Kiwis","Abricots",
-				"Bananes","Cerises"};
-		//à compléter :
+        String[] fruits = {"Poires", "Figues", "Grenadines", "Prunes", "Mangues",
+                "Raisins", "Mandarines", "Pommes", "Melons", "Kiwis", "Abricots",
+                "Bananes", "Cerises"};
+        //à compléter :
 
         //Triage
         int i, j, smallest;
         String temp;
         String output1 = "";
+        String cmd = "";
+        Scanner clavier = new Scanner(System.in);
 
         for (i = 0; i < fruits.length - 1; ++i) {
             smallest = i;
@@ -29,10 +32,30 @@ public class Programme2 {
 
         for (i = 0; i < fruits.length; i++) {
             output1 += fruits[i];
-            if (i < fruits.length-1)
+            if (i < fruits.length - 1)
                 output1 += ";";
         }
         System.out.println("[" + output1 + "]");
 
-	}
+        int pos = -1;
+
+        while (!cmd.equals("fin")) {
+            System.out.println("Rechercher un fruit ou entrer 'fin' pour quitter");
+            cmd = clavier.nextLine();
+            for (int k = 0; k < fruits.length - 1; k++) {
+                if (fruits[k].equalsIgnoreCase(cmd)) {
+                    pos = k;
+                }
+            }
+            if (pos == -1)
+                System.out.println("[" + cmd + "] n'est pas dans la liste.");
+            else
+                System.out.println("[" + cmd + "] se trouve à la position " + pos);
+
+        }
+
+
+
+    }
+
 }
