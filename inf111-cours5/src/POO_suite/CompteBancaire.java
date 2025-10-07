ae0f8f5a608c4d01623b02c1e07b1e266f997c98
Ndiaga Cisse <cndiaga2002@gmail.com>
2025-10-07 10:38:04 -0400

    notion: Add POO_Suite

---
+++ inf111-cours5/src/POO_suite/CompteBancaire.java
@@ -0,0 +1,24 @@
+package POO_suite;
+
+public class CompteBancaire {
+    private String numero;
+    private String titulaire;
+    private double solde;
+
+    //On donne un sens à l'égalité des objets: quand est-ce qu'ils sont égaux ?
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (obj instanceof CompteBancaire) {
+            CompteBancaire cpt = (CompteBancaire) obj;
+            return this.numero.equals(cpt.numero); //l'égalité est base sur l'identité des objets: leurs numeros de compte dans cas
+        }
+        else
+            return false;
+    }
+}
